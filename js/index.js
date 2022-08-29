39ab669ce1f5c47c653a417f01476eb190b15549
agustinnarciande <narciande.2019@gmail.com>
2022-08-29 17:25:12 -0300

    actualizando proyecto

---
+++ js/index.js
@@ -0,0 +1,251 @@
+class Producto {
+    constructor(id, nombre, precio, foto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.foto = foto;
+    }
+}
+
+class ElementoCarrito {
+    constructor(producto, cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+}
+
+/**
+ * Definiciones de constantes
+ */
+const estandarDolaresAmericanos = Intl.NumberFormat('en-US');
+
+//Arrays donde guardaremos catálogo de productos y elementos en carrito
+const productos = [];
+let elementosCarrito = [];
+
+const contenedorProductos = document.getElementById('contenedor-productos');
+
+const contenedorCarritoCompras = document.querySelector("#items")
+
+const contenedorFooterCarrito = document.querySelector("#footer");
+
+/**
+ * Ejecución de funciones
+ */
+
+cargarProductos();
+cargarCarrito();
+dibujarCarrito();
+dibujarCatalogoProductos();
+
+/**
+ * Definiciones de funciones
+ */
+
+function cargarProductos() {
+    productos.push(new Producto(001, "Mate calabaza forrado estilo imperial", 1300, "./img/mate_calabaza.jpg"));
+    productos.push(new Producto(002,"Mate forrado sintetico tipo Pampa xl negro", 1400, "./img/mate_forrado_tipo_pampa.jpg"));
+    productos.push(new Producto(003,"Mate Lalo termico de PVC engomado", 1030, "./img/mate_lalo.jpg",));
+    productos.push(new Producto(004,"Mate olivia cero termico", 950, "./img/mate_olivia.jpg", ));
+    productos.push(new Producto(005,"Mate tipo Stanley con bombilla y caja de regalo", 2200, "./img/mate_pampa.jpg",));
+    productos.push(new Producto(006,"Mate Pampa varios colores con bombilla y estuche", 1470, "./img/mate_tipo_stanley.jpg",));
+}
+
+function cargarCarrito() {
+    /*let elementoCarrito = new ElementoCarrito(
+        new Producto(1, 'Muffin', 1.99, './img/muffin.jpg'),
+        1
+    );
+
+    elementosCarrito.push(elementoCarrito);*/
+}
+
+function dibujarCarrito() {
+
+    let sumaCarrito = 0;
+    contenedorCarritoCompras.innerHTML = "";
+
+    
+    localStorage.setItem("elementosCarrito" , JSON.stringify(elementosCarrito));
+
+     if(localStorage.getItem('elementosCarrito')===null){
+         elementosCarrito = []
+     }else{
+         elementosCarrito = JSON.parse(localStorage.getItem('elementosCarrito'));
+     }
+
+    
+   // localStorage.setItem("elementosCarrito") === null ? elementosCarrito = [] : elementosCarrito = JSON.parse(localStorage.getItem('elementosCarrito'));
+     
+     
+
+    elementosCarrito.forEach(
+        (elemento) => {
+            let renglonesCarrito= document.createElement("tr");
+            
+            renglonesCarrito.innerHTML = `
+                <td>${elemento.producto.id}</td>
+                <td>${elemento.producto.nombre}</td>
+                <td><input id="cantidad-producto-${elemento.producto.id}" type="number" value="${elemento.cantidad}" min="1" max="1000" step="1" style="width: 50px;"/></td>
+                <td>$ ${elemento.producto.precio}</td>
+                <td>$ ${estandarDolaresAmericanos.format(elemento.producto.precio*elemento.cantidad)}</td>
+                <td><button id="eliminar-producto-${elemento.producto.id}" type="button" class="btn btn-danger"><i class="bi bi-trash-fill"></i></button></td>
+            `;
+
+            contenedorCarritoCompras.append(renglonesCarrito);
+
+            sumaCarrito+=elemento.cantidad*elemento.producto.precio;
+           
+
+            //agregamos evento a carrito
+            let cantidadProductos = document.getElementById(`cantidad-producto-${elemento.producto.id}`);
+            
+            cantidadProductos.addEventListener("change", (e) => {
+                let nuevaCantidad = e.target.value;
+                elemento.cantidad = nuevaCantidad;
+                dibujarCarrito();
+            });
+
+            let borrarProducto = document.getElementById(`eliminar-producto-${elemento.producto.id}`);
+
+            borrarProducto.addEventListener("click", (e) => {
+                removerProductoCarrito(elemento);
+                dibujarCarrito();
+            });
+
+        }
+    ); 
+
+
+    //contenedorCarritoCompras.innerHTML = renglonesCarrito;
+    
+    if(elementosCarrito.length == 0) {
+        contenedorFooterCarrito.innerHTML = `
+            <th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>
+        `;
+    } else {
+        contenedorFooterCarrito.innerHTML = `
+            <th scope="row" colspan="5">Total de la compra: $${estandarDolaresAmericanos.format(sumaCarrito)}</th>
+        `;
+    }
+    
+    
+
+}
+
+function removerProductoCarrito(elementoAEliminar) {
+    const elementosAMantener = elementosCarrito.filter((elemento) => elementoAEliminar.producto.id != elemento.producto.id);
+    elementosCarrito.length = 0;
+
+    elementosAMantener.forEach((elemento) => elementosCarrito.push(elemento));
+}
+
+function crearCard(producto) {
+    //Botón
+    let botonAgregar = document.createElement("button");
+    botonAgregar.className = "btn btn-success";
+    botonAgregar.innerText = "Agregar";
+
+    //Card body
+    let cuerpoCarta = document.createElement("div");
+    cuerpoCarta.className = "card-body";
+    cuerpoCarta.innerHTML = `
+        <h3>${producto.nombre}</h3>
+        <p>$ ${producto.precio} ARS</p>
+    `;
+    cuerpoCarta.append(botonAgregar);
+
+    //Imagen
+    let imagen = document.createElement("img");
+    imagen.src = producto.foto;
+    imagen.className = "card-img-top";
+    imagen.alt = producto.nombre;
+
+    //Card
+    let carta = document.createElement("div");
+    carta.className = "card m-2 p-2";
+    carta.style = "width: 18rem";
+    carta.append(imagen);
+    carta.append(cuerpoCarta);
+
+    //Contenedor Card
+    //let contenedorCarta = document.createElement("div");
+    //contenedorCarta.className = "col-xs-6 col-sm-3 col-md-2";
+    //contenedorCarta.append(carta);
+
+    //Agregar algunos eventos
+    botonAgregar.onclick = () => {
+        //alert("Hiciste click en el producto" + producto.nombre);
+
+        let elementoExistente = elementosCarrito.find((elemento) => elemento.producto.id == producto.id);
+
+        if(elementoExistente) {
+            elementoExistente.cantidad+=1;
+        } else {
+            let elementoCarrito = new ElementoCarrito(producto, 1);
+            elementosCarrito.push(elementoCarrito);
+        }
+
+        dibujarCarrito();
+
+        swal({
+            title: "¡Producto agregado!",
+            text: `${producto.nombre} agregado al carrito de compra.`,
+            icon: "success",
+            buttons: {
+                cerrar: {
+                    text: "Cerrar",
+                    value: false
+                },
+                carrito: {
+                    text: "Ir a carrito",
+                    value: true
+                }
+            }
+        }).then((irACarrito) => {
+
+            if(irACarrito) {
+                //swal("Vamos al carrito!");
+                const myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {keyboard: true});
+                const modalToggle = document.getElementById('toggleMyModal'); 
+                myModal.show(modalToggle);
+
+            }
+        });
+
+    } 
+    
+    return carta;
+
+}
+
+function dibujarCatalogoProductos() {
+    contenedorProductos.innerHTML = "";
+
+    productos.forEach(
+        (producto) => {
+            let contenedorCarta = crearCard(producto);
+            contenedorProductos.append(contenedorCarta);
+        }
+    );
+
+}   
+
+
+
+    // localStorage.setItem("elementosCarrito" , JSON.stringify(elementosCarrito))
+
+
+// function addLocalStorage(){
+//     localStorage.setItem('elementosCarrito' , JSON.stringify(elementosCarrito))
+// }
+
+
+// window.onload = function (){
+//     const storage = localStorage.getItem('elementosCarrito');
+//     if(storage){
+//        carrito = storage;
+//        dibujarCarrito()
+//     }
+// }
+
